ea4f048e53d23b3ae8ecd76f1ea1a1133a1c8e15
arifzxc <113068651+arifzxc@users.noreply.github.com>
2022-10-11 23:10:55 +0700

    Create ubuntu.sh

---
+++ nginx/ubuntu.sh
@@ -0,0 +1,7 @@
+# Database menggunakan mysql
+
+sudo apt install nginx php php-xmlrpc php-mysql php-gd php-cli php-curl \
+mysql-client mysql-server libphp-adodb libgd-dev php-pear \
+php-common libapache2-mod-php php-fpm \
+php-bz2 php-xml imagemagick php-zip php-mbstring  -y
+

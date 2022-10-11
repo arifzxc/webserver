a515734b5b00af859651efa46349d92c93e1cd41
arifzxc <113068651+arifzxc@users.noreply.github.com>
2022-10-11 22:59:33 +0700

    Create debian.sh

---
+++ apache2/debian.sh
@@ -0,0 +1,6 @@
+# Database pakek mariadb
+
+apt install apache2 php php-xmlrpc php-mysql php-gd php-cli php-curl \
+mariadb-client mariadb-server libphp-adodb libgd-dev php-pear \
+php-common libapache2-mod-php php-fpm \
+php-bz2 php-xml imagemagick php-zip php-mbstring -y 

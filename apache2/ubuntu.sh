2ee6e9b02b2daa74af6933aae91e5cbae75cc09b
arifzxc <113068651+arifzxc@users.noreply.github.com>
2022-10-11 23:00:31 +0700

    Update ubuntu.sh

--- apache2/ubuntu.sh
+++ apache2/ubuntu.sh
@@ -1,3 +1,5 @@
+# Database pakek mysql
+
 apt install apache2 php php-xmlrpc php-mysql php-gd php-cli php-curl \
 mysql-client mysql-server libphp-adodb libgd-dev php-pear \
 php-common libapache2-mod-php php-fpm \

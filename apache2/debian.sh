a039a13d37a2342a11db6a7de99f0521dc60fbd2
arifzxc <113068651+arifzxc@users.noreply.github.com>
2022-10-16 00:13:55 +0700

    Update debian.sh

--- apache2/debian.sh
+++ apache2/debian.sh
@@ -5,3 +5,4 @@ mariadb-client mariadb-server libphp-adodb libgd-dev php-pear \
 php-common libapache2-mod-php php-fpm \
 php-bz2 php-xml imagemagick php-zip php-mbstring -y 
 
+/etc/init.d/apache2 restart

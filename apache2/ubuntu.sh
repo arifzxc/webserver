bb40696082498608c855f972257e0978a07ce34c
arifzxc <113068651+arifzxc@users.noreply.github.com>
2022-10-16 00:14:51 +0700

    Update ubuntu.sh

--- apache2/ubuntu.sh
+++ apache2/ubuntu.sh
@@ -5,3 +5,5 @@ mysql-client mysql-server libphp-adodb libgd-dev php-pear \
 php-common libapache2-mod-php php-fpm \
 php-bz2 php-xml imagemagick php-zip php-mbstring  -y
 
+/etc/init.d/apache2 restart
+
